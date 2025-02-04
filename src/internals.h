915f92c4047d9bbee1966a95f0b04bf5e5bc17eb
zakariamoknine <zakariamoknine3@gmail.com>
2025-02-04 19:26:55 +0100

    Update

--- src/internals.h
+++ src/internals.h
@@ -14,13 +14,13 @@
 * Platform
 */
 #if defined(_WIN32)
- #define PXL_PLATFORM_WIN32
+    #define PXL_PLATFORM_WIN32
 #elif defined(__linux__)
- #define PXL_PLATFORM_LINUX
+    #define PXL_PLATFORM_LINUX
 #elif defined(__APPLE__)
- #define PXL_PLATFORM_APPLE
+    #define PXL_PLATFORM_APPLE
 #else
- #error "unknown/unsupported platform"
+    #error "unknown/unsupported platform"
 #endif
 
 
@@ -28,13 +28,13 @@
 * Thread local definition
 */
 #if defined(__cplusplus) &&  __cplusplus >= 201103L
- #define PXL_THREAD_LOCAL thread_local
+    #define PXL_THREAD_LOCAL thread_local
 #elif defined(__GNUC__)
- #define PXL_THREAD_LOCAL __thread
+    #define PXL_THREAD_LOCAL __thread
 #elif defined(_MSC_VER)
- #define PXL_THREAD_LOCAL __declspec(thread)
+    #define PXL_THREAD_LOCAL __declspec(thread)
 #elif defined (__STDC_VERSION__) && __STDC_VERSION__ >= 201112L && !defined(__STDC_NO_THREADS__)
- #define PXL_THREAD_LOCAL _Thread_local
+    #define PXL_THREAD_LOCAL _Thread_local
 #endif
 
 
@@ -65,5 +65,6 @@ typedef enum PXLresult
 void _pxlInputError(const char* description);
 
 const char* pxlGetError();
+
 
 #endif // _PXL_INTERNALS_H_
